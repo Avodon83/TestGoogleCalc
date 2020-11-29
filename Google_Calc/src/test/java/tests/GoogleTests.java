4cc07b3822fb72a89bd788ff7bb2ddc12e6aa774
Aleksandr Fadeev <avodon@mail.ru>
2020-11-29 19:42:04 +0400

    Управление калькулятором оформляем page классом

--- Google_Calc/src/test/java/tests/GoogleTests.java
+++ Google_Calc/src/test/java/tests/GoogleTests.java
@@ -1,16 +1,38 @@
 package tests;
 
+import org.junit.After;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GoogleTests {
 
-    @Test
-    public void test1(){
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void init(){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    @DisplayName("Поиск калькулятора")
+    public void test1(){
         driver.get("https://google.com");
+        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор", Keys.ENTER);
+    }
+
+    @AfterAll
+    public static void shutdown(){
         driver.quit();
     }
+
 }
